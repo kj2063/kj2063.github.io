d864a606f2d85d63f3e3866a00432a89effb253d
Jun Kwon <kwonjun8890@gmail.com>
2023-05-13 18:45:00 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/posts`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
