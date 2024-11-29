db2443d21c6f40c03d8e945c00574c08163bfd4e
Jun Kwon <kwonjun8890@gmail.com>
2024-11-29 15:54:33 +0900

    change browser icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,15 +35,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#006666`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/star-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
