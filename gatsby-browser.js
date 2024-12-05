14cb610fb41de62e3401d2aa36d39ec0141531c9
Jun Kwon <kwonjun8890@gmail.com>
2024-12-05 16:58:17 +0900

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,26 +4,35 @@
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/
  */
 
-import React from "react"
+import React, { useEffect } from "react"
 
 import { ThemeProvider } from "@src/context/ThemeContext"
 import Layout from "@src/components/layout"
 
-export const wrapRootElement = ({ element }) => (
-    <ThemeProvider><Layout>{element}</Layout></ThemeProvider>
-)
+export const wrapRootElement = ({ element }) => {
+    return <ThemeProvider>{element}</ThemeProvider>
+}
+
+export const wrapPageElement = ({ element, props }) => {
+    const ScrollToTop = ({ children }) => {
+        useEffect(() => {
+            window.scrollTo(0, 0);
+        }, [props.location.pathname]);
+
+        return <>{children}</>;
+    };
+
+    return (
+        <ScrollToTop>
+            <Layout>{element}</Layout>
+        </ScrollToTop>
+    )
+};
 
 export const onRouteUpdate = ({ location, prevLocation }) => {
     /* url 마지막 '/' 제거 */
     if (location.pathname.endsWith('/') && location.pathname !== '/') {
         const trimmedPath = location.pathname.slice(0, -1);
         window.history.replaceState({}, '', trimmedPath);
-    }
-
-    /* 페이지 조회시 항상 스크롤 상단으로 이동 */
-    if (location.pathname !== prevLocation?.pathname) {
-        requestAnimationFrame(() => {
-            window.scrollTo(0, 0);
-        })
     }
 };
