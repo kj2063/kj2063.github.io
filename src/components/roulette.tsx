8b770927c954a252cfd5b0f8c261f1ec2cf25526
Jun Kwon <kwonjun8890@gmail.com>
2025-02-04 17:25:35 +0900

    add roulette

---
+++ src/components/roulette.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import '@src/styles/roulette.css';
+
+const Roulette = () => {
+  const items = [
+    '학습 및 포스팅',
+    '코딩테스트 문제풀이',
+    '개인 프로젝트 진행',
+    '블로그 확장 개발',
+  ];
+  const itemsRepeatCnt = 5;
+  const itemHeight = 60; // 개별 아이템 높이 - css도 맞춰줘야 함
+  const [position, setPosition] = useState(0);
+  const [isSpinning, setIsSpinning] = useState(false);
+
+  let totalItems : string[] = [];
+
+  for (let i = 0; i < itemsRepeatCnt; i += 1) {
+    totalItems = totalItems.concat(items);
+  }
+
+  const spinRoulette = () => {
+    if (isSpinning) return;
+
+    /* css - transition 제거 */
+    const itemsElement : any = document.querySelector('.roulette-items');
+    if (itemsElement) {
+      itemsElement.style.transition = 'none';
+      setPosition(0);
+    }
+
+    setIsSpinning(true);
+
+    const finalIndex = (itemsRepeatCnt - 1) * items.length + Math.floor(Math.random() * items.length);
+    const finalPosition = -(finalIndex * itemHeight);
+
+    setTimeout(() => {
+      if (itemsElement) {
+        itemsElement.style.transition = 'transform 1s ease-out';
+        setPosition(finalPosition);
+      }
+
+      setIsSpinning(false);
+    }, 0);
+  };
+
+  return (
+    <div className="roulette-container">
+      <div className="roulette-title">
+        <h3>✏오늘의 개발공부✏</h3>
+      </div>
+      <div className="roulette-content">
+        <div className="roulette">
+          <div className="roulette-items" style={{ transform: `translateY(${position}px)`, transition: 'transform 1s ease-out' }}>
+            {totalItems.map((item, index) => (
+              <div key={index} className="roulette-item">{item}</div>
+            ))}
+          </div>
+        </div>
+        <button type="button" className="roulette-button" onClick={spinRoulette} disabled={isSpinning}>
+          {isSpinning ? 'Spinning...' : 'Start'}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Roulette;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Seo from '@src/components/seo';
+import Roulette from '@src/components/roulette';
 import '@src/styles/common.css';
 import '@src/styles/index.css';
 
@@ -9,14 +10,7 @@ const IndexPage = () => (
       Home
     </h2>
     <div className="textCenter">
-      <br />
-      <br />
-      <h3>Jun's 블로그에 오신것을 환영합니다!</h3>
-      <br />
-      <b>현재 Home은 개발중에 있습니다.</b>
-      <br />
-      <br />
-      <br />
+      <Roulette />
     </div>
   </div>
 );
